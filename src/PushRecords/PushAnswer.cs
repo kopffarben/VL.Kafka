c277aac40c86d83b7473e1f19ff11eab82f0fd74
Johannes Schmidt <schmidt@kopffarben.de>
2023-02-24 16:21:27 +0100

    PushAnswer JsonSerialize and JsonDesertialize are working

--- src/PushRecords/PushAnswer.cs
+++ src/PushRecords/PushAnswer.cs
@@ -4,14 +4,19 @@ namespace StreamRecords
 {
     public record PushAnswer
     {
-        public string de { get; }
-        public string en { get; }
-        public string SessionID { get; }
-        public Language language { get; }
-        public string Timestamp { get; }
-        public int Question { get; }
-        public lang[] Keywords { get; }
-        public int[] Categories { get; }
+        public string de { get; set; }
+        public string en { get; set; }
+        public string SessionID { get; set; }
+        public Language language { get; set; }
+        public string Timestamp { get; set; }
+        public int Question { get; set; }
+        public lang[] Keywords { get; set; }
+        public int[] Categories { get; set; }
+
+        public PushAnswer()
+        {
+
+        }
 
         public  PushAnswer(string de, string en, string language, string SessionID, Int64 timestamp, int Question, Spread<lang> Keywords, Spread<int> Categories)
         {
@@ -34,6 +39,28 @@ namespace StreamRecords
             this.Question = Question;
             this.Keywords = Keywords.GetInternalArray();
             this.Categories = Categories.GetInternalArray();
+        }
+        public PushAnswer(string de, string en, string language, string SessionID, Int64 timestamp, int Question, lang[] Keywords, int[] Categories)
+        {
+            this.de = de;
+            this.en = en;
+            switch (language)
+            {
+                case "de":
+                    this.language = Language.german;
+                    break;
+                case "en":
+                    this.language = Language.english;
+                    break;
+                default:
+                    this.language = Language.none;
+                    break;
+            }
+            this.SessionID = SessionID;
+            this.Timestamp = timestamp.ToString();
+            this.Question = Question;
+            this.Keywords = Keywords;
+            this.Categories = Categories;
         }
     }
 
